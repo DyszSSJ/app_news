d5ed934ce24ff25b5694031d1ed29f1d688d9357
AxelDavidTellezA <gtacuenta47@gmail.com>
2024-01-16 20:18:49 -0600

    feat: :fire: nuevo diseño
    se hizo un nuevo diseño en la screen de details

--- src/screens/detailScreen.tsx
+++ src/screens/detailScreen.tsx
@@ -9,9 +9,15 @@ interface DetailScreenProps {
 export default function DetailScreen({route}: DetailScreenProps) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{route.params.title}</Text>
-      <Text style={styles.description}>{route.params.description}</Text>
-      <Image source={{uri: route.params.urlToImage}} style={styles.image} />
+      <View>
+        <View style={styles.imageContainer}>
+          <Image source={{uri: route.params.urlToImage}} style={styles.image} />
+        </View>
+        <View>
+          <Text style={styles.title}>{route.params.title}</Text>
+          <Text style={styles.description}>{route.params.description}</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -19,24 +25,39 @@ export default function DetailScreen({route}: DetailScreenProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     backgroundColor: '#fff',
-    padding: 20,
+  },
+  imageContainer: {
+    borderBottomEndRadius: 40,
+    borderBottomStartRadius: 40,
+    overflow: 'hidden',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 20,
+    elevation: 20,
+    backgroundColor: '#fff',
+    marginBottom: 15,
+  },
+  image: {
+    width: 'auto',
+    height: 400,
+    resizeMode: 'cover',
   },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color: '#333',
+    fontSize: 21,
+    fontWeight: '700',
+    padding: 10,
+    color: '#000',
   },
   description: {
     fontSize: 16,
-    color: '#666',
-    marginBottom: 20,
-  },
-  image: {
-    width: '100%',
-    height: 200,
-    borderRadius: 10,
-    marginBottom: 20,
+    fontWeight: '400',
+    padding: 20,
+    color: '#7c7c7c',
   },
 });
